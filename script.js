b2617ff3b92ad8443f1ca4c9389083e20716c9e7
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-10 22:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let f1 = 0
+	let f2 = 1
+	let f3 = 0
+	for(let i = 0; i < num - 2; i++)
+		{
+			f3 = f1 + f2
+			f1 = f2
+			f2 = f1
+		}
+	return f3
 }
 
 module.exports = fibonacci;
